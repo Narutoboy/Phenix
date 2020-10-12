7837cf7121bf947bbd781ae0ba1e2440991452d0
Narutoboy <akshay.sharma21111995@gmail.com>
2020-10-12 22:42:57 +0530

    Removed extra layout files

--- app/src/main/java/com/do_big/teamphenix/SplashScreen.java
+++ app/src/main/java/com/do_big/teamphenix/SplashScreen.java
@@ -21,46 +21,47 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 
         //overridePendingTransition(R.anim.left,R.anim.fadeout);
-        TextView title = (TextView) findViewById(R.id.txt_appName);
-        ImageView image = (ImageView) findViewById(R.id.img_appIcon);
+        TextView title = findViewById(R.id.txt_appName);
+        ImageView image = findViewById(R.id.img_appIcon);
         //Animation animation= AnimationUtils.loadAnimation(SplashScreen.this, R.anim.fadein);
         //Animation animimage= AnimationUtils.loadAnimation(SplashScreen.this, R.anim.rotate);
         //title.startAnimation(animation);
         //image.startAnimation(animimage);
         PrefManager prefManager = new PrefManager(this);
         if (prefManager.isFirstTimeLaunch()) {
-            TimerTask tt=new TimerTask() {
+            TimerTask tt = new TimerTask() {
                 @Override
                 public void run() {
                     //for first time
-                    //lunch login screen
-                    startActivity(new Intent(SplashScreen.this,BaseActivity.class));
+                    //launch login screen
+                    startActivity(new Intent(SplashScreen.this, BaseActivity.class));
                     SplashScreen.this.finish();
 
                 }
             };
-            Timer t=new Timer();
-            t.schedule(tt,1000);
+            Timer t = new Timer();
+            t.schedule(tt, 1000);
 
-        }
-        else{
-            TimerTask tt=new TimerTask() {
+        } else {
+            TimerTask tt = new TimerTask() {
                 @Override
                 public void run() {
                     //lunch Dashboard Activity
-                    Intent intent=new Intent(SplashScreen.this,BaseActivity.class);
+                    Intent intent = new Intent(SplashScreen.this, BaseActivity.class);
                     startActivity(intent);
                     SplashScreen.this.finish();
                 }
             };
-            Timer t=new Timer();
-            t.schedule(tt,2500);
+            Timer t = new Timer();
+            t.schedule(tt, 2500);
 
 
         }
 
     }
+
+
 }
